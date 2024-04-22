08049f3cf6afffe79b0b6463a4561348ef5ee95e
Ritej Belgacem <ritejbelqacem111@gmail.com>
2024-04-22 14:59:51 +0100

    Creation of MixController class

---
+++ src/Controller/MixController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MixController extends AbstractController
+{
+    #[Route('/mix/new')]
+    public function new(): Response
+    {
+        dd('new mix');
+    }
+}
